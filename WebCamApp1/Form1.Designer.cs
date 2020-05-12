92d47d202dee256ff1b10aa0bf34eb29f07e0549
Askar <ascar1@yandex.ru>
2020-05-12 17:58:54 +0500

    12052020

--- WebCamApp1/Form1.Designer.cs
+++ WebCamApp1/Form1.Designer.cs
@@ -28,15 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 279);
+            this.button1.Location = new System.Drawing.Point(12, 362);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -46,27 +55,85 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 279);
+            this.button2.Location = new System.Drawing.Point(93, 362);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button2_KeyDown);
+            this.button2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.button2_KeyPress);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 59);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(244, 261);
+            this.pictureBox1.Size = new System.Drawing.Size(449, 297);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(716, 362);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Go";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(494, 59);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(216, 297);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(716, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(301, 297);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(716, 391);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "GetPDF";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1182, 580);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -74,7 +141,10 @@
             this.Text = "Form1";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +152,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- WebCamApp1/Form1.cs
+++ WebCamApp1/Form1.cs
@@ -1,5 +1,10 @@
-﻿using OpenCvSharp;
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using OpenCvSharp;
 using OpenCvSharp.Extensions;
+
+using PdfSharp;
+using PdfSharp.Pdf;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,10 +15,13 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
 
 namespace WebCamApp1
 {
+
+
     public partial class Form1 : Form
     {
         VideoCapture capture;
@@ -22,8 +30,12 @@ namespace WebCamApp1
         private Thread camera;
         bool isCameraRunning = false;
 
+        BindingSource source = new BindingSource();
+
         private void CaptureCamera()
         {
+
+
             camera = new Thread(new ThreadStart(CaptureCameraCallback));
             camera.Start();
         }
@@ -32,21 +44,15 @@ namespace WebCamApp1
         {
 
             frame = new Mat();
-            capture = new VideoCapture(0);
-            //capture.Set(CaptureProperty.FrameHeight, 2000);
-            //capture.Set(CaptureProperty.FrameHeight, 1000);
-            
-            
-            capture.Open(0);
-
             //MessageBox.Show(capture.FrameHeight.ToString());
             if (capture.IsOpened())
             {
-                while (isCameraRunning)
-                {
-                    capture.FrameWidth = 1280;
-                    capture.FrameHeight = 1044;
-
+                capture.FrameWidth = 1280;
+                capture.FrameHeight = 720;
+                capture.AutoFocus = true;
+                
+               while (isCameraRunning)
+               {
 
                     capture.Read(frame);
                     image = BitmapConverter.ToBitmap(frame);
@@ -56,17 +62,24 @@ namespace WebCamApp1
                     }
                     pictureBox1.Image = image;
                     
-                }
+               }
             }
         }
 
         public Form1()
         {
             InitializeComponent();
+            source.DataSource = pages;
+            dataGridView1.DataSource = source;
+
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            capture = new VideoCapture(0);
+            capture.Open(0);
             if (button1.Text.Equals("Start"))
             {
                 CaptureCamera();
@@ -75,6 +88,7 @@ namespace WebCamApp1
             }
             else
             {
+                camera.Abort();
                 capture.Release();
                 button1.Text = "Start";
                 isCameraRunning = false;
@@ -83,17 +97,74 @@ namespace WebCamApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
+            TakePhoto();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (camera!= null)
+            {
+                camera.Abort();
+            }
+           
+            
+        }
+
+        static System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        private void button3_Click(object sender, EventArgs e)
+        {
+            isCameraRunning = true;
+            myTimer.Tick += new EventHandler(OnTimedEvent);
+            myTimer.Interval = 3000;
+            
+            if( myTimer.Enabled)
+            {
+                myTimer.Stop();
+                myTimer.Dispose();
+                button3.Text = "Start";
+            }
+            else
+            {
+                //camera.Abort ();
+                myTimer.Start();
+                button3.Text = "Stop";
+                
+            }
+        }
+
+        private void OnTimedEvent(Object myObject, EventArgs myEventArgs)
+        {            
+            this.TakePhoto();
+            //MessageBox.Show("!");
+        }
+
+        List<Page> pages = new List<Page>();
+        int i = 1;
+        private void TakePhoto ()
+        {
             if (isCameraRunning)
             {
+                frame = new Mat();
+                capture.Read(frame);
+
+                
                 // Take snapshot of the current image generate by OpenCV in the Picture Box
-                Bitmap snapshot = new Bitmap(pictureBox1.Image);
+                Bitmap snapshot = BitmapConverter.ToBitmap(frame);
+                snapshot.RotateFlip(RotateFlipType.Rotate90FlipXY);
+                if (pictureBox2.Image != null)
+                {
+                    pictureBox2.Image.Dispose();
+                }
+                pictureBox2.Image = snapshot;
 
-                // Save in some directory
-                // in this example, we'll generate a random filename e.g 47059681-95ed-4e95-9b50-320092a3d652.png
-                // snapshot.Save(@"C:\Users\sdkca\Desktop\mysnapshot.png", ImageFormat.Png);
-                snapshot.Save(string.Format(@"C:\2\{0}.png", Guid.NewGuid()), ImageFormat.Png);
-                image.Save(string.Format(@"C:\2\{0}.png", Guid.NewGuid()), ImageFormat.Png);
-
+                string name = string.Format(@"C:\2\{0}{1}.jpeg", textBox1.Text, i);
+                snapshot.Save(name, ImageFormat.Jpeg);
+                
+                source.Add(new Page() { Num = i, FName = name });
+                i++;
+                /*name = string.Format(@"C:\2\{0}.jpeg", Guid.NewGuid());
+                snapshot.Save(name, ImageFormat.Jpeg);*/
+                
             }
             else
             {
@@ -101,11 +172,80 @@ namespace WebCamApp1
             }
         }
 
-        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        private void tmp123 (Object tmp)
         {
-            camera.Abort();
-            
-           
+            MigraDoc.DocumentObjectModel.Shapes.Image image ;
+            image = (MigraDoc.DocumentObjectModel.Shapes.Image) tmp;
+            image.Height = "297mm";
+            image.Width = "210mm";
         }
+
+        private void GetPDF ()
+        {
+            string name = textBox1.Text + ".pdf";
+            Document document = new Document();
+            Section section = document.AddSection();
+            section.PageSetup.PageFormat = PageFormat.A4;//стандартный размер страницы
+            section.PageSetup.Orientation = MigraDoc.DocumentObjectModel.Orientation.Portrait;//ориентация
+            section.PageSetup.BottomMargin = 0;//нижний отступ
+            section.PageSetup.TopMargin = 0;//верхний отступ
+            section.PageSetup.RightMargin = 0; //
+            section.PageSetup.LeftMargin = 0;
+
+
+            foreach (Page page in pages)
+            {
+                //MigraDoc.DocumentObjectModel.Shapes.Image image = new MigraDoc.DocumentObjectModel.Shapes.Image(page.FName)
+                Paragraph paragraph = new Paragraph();               
+                
+                paragraph.AddImage (page.FName);
+                var tmp = paragraph.Elements.LastObject;
+                if (tmp is MigraDoc.DocumentObjectModel.Shapes.Image)
+                {
+                    tmp123(tmp); 
+                    
+                    
+                }
+                
+                
+                section.Add(paragraph);
+                
+            }
+            
+
+            PdfDocumentRenderer pdfRenderer = new PdfDocumentRenderer(true);
+            pdfRenderer.Document = document;
+            pdfRenderer.RenderDocument();
+            string name1 = string.Format(@"C:\2\{0}.pdf", Guid.NewGuid());
+            pdfRenderer.PdfDocument.Save(name1);
+            MessageBox.Show(name1);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            GetPDF();
+        }
+
+        private void button2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                TakePhoto();
+            }
+        }
+    }
+
+
+
+    class Page
+    {
+        public int Num { get; set; }
+        public string FName { get; set; }
+
     }
 }
